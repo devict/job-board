c8090e6f726f22ea89988959bdb6609c08614302
Seth Etter <mail@sethetter.com>
2022-05-07 19:32:58 -0500

    Tests for home view and new job form

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -3,10 +3,12 @@ package server_test
 import (
 	"database/sql"
 	"database/sql/driver"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	"regexp"
 	"testing"
 	"time"
 
@@ -18,12 +20,102 @@ import (
 )
 
 // Things to test:
-// - create a new job
-// - view a job
-// - view all jobs
-// - update a job
-// - unauthorized access to job edit (view and post)
 // - job deleted after 30 days?
+
+func TestIndex(t *testing.T) {
+	s, _, dbmock := makeServer(t)
+	defer s.Close()
+
+	rows := sqlmock.NewRows(getDbFields(data.Job{})).
+		AddRow(mockJobRow(data.Job{Position: "Pos 1"})...).
+		AddRow(mockJobRow(data.Job{Position: "Pos 2"})...)
+	dbmock.ExpectQuery(`SELECT \* FROM jobs`).WillReturnRows(rows)
+
+	body, _ := sendRequest(t, s.URL)
+
+	assert.Contains(t, string(body), "Pos 1")
+	assert.Contains(t, string(body), "Pos 2")
+
+	// TODO: What other assertions do we want to make about the home page?
+}
+
+func TestNewJob(t *testing.T) {
+	s, _, _ := makeServer(t)
+	defer s.Close()
+
+	body, _ := sendRequest(t, fmt.Sprintf("%s/new", s.URL))
+
+	// - assert that all the right fields are present
+	tests := []struct {
+		field    string
+		required bool
+		textArea bool
+	}{
+		{"position", true, false},
+		{"organization", true, false},
+		{"description", false, true},
+		{"url", false, false},
+		{"email", true, false},
+	}
+
+	for _, tt := range tests {
+		reqStr := ""
+		if tt.required {
+			reqStr = "required.*"
+		}
+
+		base := "input"
+		if tt.textArea {
+			base = "textarea"
+		}
+
+		r := fmt.Sprintf(`<%s.+name="%s".*%s>`, base, tt.field, reqStr)
+		assert.Regexp(t, regexp.MustCompile(r), string(body))
+	}
+}
+
+func TestCreateJob(t *testing.T) {
+	// TODO
+	// - post some form data to the create job page
+	// - assert the right sql insert query is called
+	// - assert email sent
+	// - assert slack and twitter posts
+}
+
+func TestViewJob(t *testing.T) {
+	// TODO
+	// - view job page
+	// - assert right SQL query is called
+	// - assert all the right data is displayed
+}
+
+func TestEditJobUnauthorized(t *testing.T) {
+	// TODO
+	// - attempt to go to edit job link
+	// - assert unauthorized response
+}
+
+func TestUpdateJobUnauthorized(t *testing.T) {
+	// TODO
+	// - attempt to post to update job link
+	// - assert unauthorized response
+}
+
+func TestEditJobAuthorized(t *testing.T) {
+	// TODO
+	// - go to edit job page with proper token
+	// - assert the select sql query
+	// - assert the fields and pre-populated values
+}
+
+func TestUpdateJobAuthorized(t *testing.T) {
+	// TODO
+	// - post data to update job route
+	// - assert sql update query
+	// - assert redirect to, wherever
+}
+
+// Helpers ------------------------------
 
 type email struct {
 	recipient, subject, body string
@@ -71,27 +163,16 @@ func makeServer(t *testing.T) (*httptest.Server, *mockService, sqlmock.Sqlmock) 
 	return testServer, svc, dbmock
 }
 
-func TestIndex(t *testing.T) {
-	s, _, dbmock := makeServer(t)
-	defer s.Close()
-
-	rows := sqlmock.NewRows(getDbFields(data.Job{})).
-		AddRow(mockJobRow(data.Job{Position: "Pos 1"})...).
-		AddRow(mockJobRow(data.Job{Position: "Pos 2"})...)
-	dbmock.ExpectQuery(`SELECT \* FROM jobs`).WillReturnRows(rows)
-
-	// make request for index route
-	resp, err := http.Get(s.URL)
+func sendRequest(t *testing.T, path string) ([]byte, *http.Response) {
+	resp, err := http.Get(path)
 	assert.NoError(t, err)
-
 	assert.Equal(t, resp.StatusCode, 200)
 
 	body, err := io.ReadAll(resp.Body)
 	assert.NoError(t, err)
 	resp.Body.Close()
 
-	assert.Contains(t, string(body), "Pos 1")
-	assert.Contains(t, string(body), "Pos 2")
+	return body, resp
 }
 
 func getDbFields(thing interface{}) []string {
